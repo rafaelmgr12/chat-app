248d118abb3b9aeb6536354aed35254196262e50
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-11-08 21:20:07 -0300

    [feat] add container of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+
+const style = {
+  appContainer: `max-w-[728px] mx-auto text-center`,
+  sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`,
+};
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello, world!</h1>;
+  
+
+
+  return (
+    <div className={style.appContainer}>
+      <section className= {style.sectionContainer}>
+        {/* Navbar */}
+        {/* Chat component */}
+      </section>
+    </div>
+  );
 }
 
 export default App;
