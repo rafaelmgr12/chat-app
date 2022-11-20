bfc4a272c8a74662aea42aff462f7fa7f69e181e
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2022-11-20 11:36:30 -0300

    [feat] add auth hooks to the app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { Navbar } from "./components/Navbar";
 
+import {auth} from "./config/firebase";
+import {useAuthState} from "react-firebase-hooks/auth";
+
+
 const style = {
   appContainer: `max-w-[728px] mx-auto text-center`,
   sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`,
 };
 
 function App() {
+
+  const [user] = useAuthState(auth);
   return (
     <div className={style.appContainer}>
       <section className={style.sectionContainer}>
